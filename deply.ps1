f6d1dfe1ea3495fb878fa09385e8f97696a57205
Sandeep-Bejjenki <bejjenki.sandeep78@gmail.com>
2021-08-16 21:59:42 +0530

    deleted

--- deply.ps1
+++
@@ -1,4 +0,0 @@
-#Connect-AzAccount
-#New-AzResourceGroup -Name "ford01rg" -Location "centralus" -Verbose
-#New-AzResourceGroupDeployment -ResourceGroupName "arm-rg1" -TemplateFile ".\master.json" -Verbose       
-New-AzResourceGroupDeployment -ResourceGroupName "ford01rg" -TemplateFile .\master.json -templateparameterfile .\node.json -mode "complete" -verbose
